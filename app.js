91e91725acfca1ea2d186cc1f83228c450873ad8
srdoyen <srdoyen@gmail.com>
2019-11-07 20:41:21 -0800

    Profile Scroller

--- app.js
+++ app.js
@@ -1,3 +1,70 @@
+const data = [
+  {
+    name: "John Doe",
+    age: 32,
+    gender: "male",
+    lookingfor: "female",
+    location: "Boston, MA",
+    image: "https://randomuser.me/api/portraits/men/82.jpg"
+  },
+  {
+    name: "Jen Smith",
+    age: 26,
+    gender: "female",
+    lookingfor: "male",
+    location: "Folsom, CA",
+    image: "https://randomuser.me/api/portraits/women/82.jpg"
+  },
+  {
+    name: "Kendrick Smith",
+    age: 42,
+    gender: "male",
+    lookingfor: "female",
+    location: "Lynn, MA",
+    image: "https://randomuser.me/api/portraits/men/81.jpg"
+  }
+];
+
+const profiles = profileIterator(data);
+
+nextProfile();
+//Next Event
+document.getElementById("next").addEventListener("click", nextProfile);
+
+//Next Profile Display
+function nextProfile() {
+  const currentProfile = profiles.next().value;
+  if (currentProfile !== undefined) {
+    document.getElementById("profileDisplay").innerHTML = `
+    <ul class="list-group">
+      <li class="list-group-item">Name: ${currentProfile.name}</li>
+      <li class="list-group-item">Age: ${currentProfile.age}</li>
+      <li class="list-group-item">Location: ${currentProfile.location}</li>
+      <li class="list-group-item">Preference: ${currentProfile.gender} Looking For: ${currentProfile.lookingfor}</li>
+    </ul>
+  `;
+
+    document.getElementById(
+      "imageDisplay"
+    ).innerHTML = `<img src="${currentProfile.image}">`;
+  } else {
+    //No more profiles
+    window.location.reload();
+  }
+}
+
+//Profile Iteration
+function profileIterator(profiles) {
+  let nextIndex = 0;
+  return {
+    next: function() {
+      return nextIndex < profiles.length
+        ? { value: profiles[nextIndex++], done: false }
+        : { done: true };
+    }
+  };
+}
+
 //Iterator Ex
 // function nameIterator(names) {
 //   let nextIndex = 0;
@@ -22,24 +89,24 @@
 // console.log(names.next());
 
 //Generator Example
-function* sayNames() {
-  yield "Jack";
-  yield "Jill";
-  yield "John";
-}
+// function* sayNames() {
+//   yield "Jack";
+//   yield "Jill";
+//   yield "John";
+// }
 
-const name = sayNames();
+// const name = sayNames();
 
-console.log(name.next().value);
+// console.log(name.next().value);
 
-//ID Creator
-function* createIds() {
-  let index = 0;
-  while (true) {
-    yield index++;
-  }
-}
+// //ID Creator
+// function* createIds() {
+//   let index = 0;
+//   while (true) {
+//     yield index++;
+//   }
+// }
 
-const gen = createIds();
+// const gen = createIds();
 
-console.log(gen.next().value);
+// console.log(gen.next().value);
